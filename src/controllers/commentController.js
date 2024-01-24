48d84a5144af348fca454604dd4fdf703dd5e465
danirus6 <44194174+danirus6@users.noreply.github.com>
2024-01-24 20:00:32 +0100

    Correcciones

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,4 +1,3 @@
-
 const Comment = require('../models/Comment');
 const Post = require('../models/Post');
 
@@ -7,28 +6,60 @@ const commentController = {
         try {
             const { postId, text } = req.body;
 
-            const postExists = await Post.findById(postId);
-            if (!postExists) {
+            const post = await Post.findById(postId);
+            if (!post) {
                 return res.status(404).send({ message: 'Post no encontrado.' });
             }
 
             const comment = new Comment({
                 text,
-                author: req.user._id,
+                author: req.user._id, // 
                 post: postId
             });
 
             await comment.save();
-
-            postExists.comments.push(comment._id);
-            await postExists.save();
-
             res.status(201).send({ message: 'Comentario agregado con éxito', comment });
         } catch (error) {
             res.status(500).send({ message: 'Error al agregar el comentario', error });
         }
     },
 
-};
+    async updateComment(req, res) {
+        try {
+            const { commentId, text } = req.body;
+            const comment = await Comment.findOneAndUpdate(
+                { _id: commentId, author: req.user._id },
+                { text },
+                { new: true }
+            );
 
+            if (!comment) {
+                return res.status(404).send({ message: 'Comentario no encontrado o no autorizado.' });
+            }
+
+            res.send({ message: 'Comentario actualizado con éxito', comment });
+        } catch (error) {
+            res.status(500).send({ message: 'Error al actualizar el comentario', error });
+        }
+    },
+    async deleteComment(req, res) {
+        try {
+            const { commentId } = req.params; // 
+
+            const comment = await Comment.findOneAndDelete({
+                _id: commentId,
+                author: req.user._id
+            });
+
+            if (!comment) {
+                return res.status(404).send({ message: 'Comentario no encontrado o no autorizado para eliminar.' });
+            }
+
+            res.send({ message: 'Comentario eliminado con éxito' });
+        } catch (error) {
+            res.status(500).send({ message: 'Error al eliminar el comentario', error });
+        }
+    },
+
+};
 module.exports = commentController;
